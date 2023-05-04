f846d9cb44e67c80aede13ada9c167fe895ecfb9
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-05-04 07:48:43 +0300

    theme.js

---
+++ src/constants/theme.js
@@ -0,0 +1,25 @@
+export const theme = {
+  colors: {
+    white: '#ffffff',
+    black: '#010101',
+    gray: 'gray',
+    lightGray: 'lightgray',
+    green: 'green',
+    red: 'red',
+
+    bgLight: '#f7f7f7',
+
+    primaryText: '#212121',
+    secondaryText: '#757575',
+
+    table: {
+      header: '#3ac3d6',
+      rowHover: 'PowderBlue',
+    },
+  },
+
+  spacing: value => `${value * 4}px`,
+  borderRadius: '4px',
+
+  boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 2px 4px 0 rgba(0, 0, 0, 0.19)',
+};
